2afddd760e1125aca2fb54463583c227fb9c7cc6
zalyora <48105531+zalyora@users.noreply.github.com>
2021-10-11 11:16:50 +0300

    folder renamed

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@ let totalCost = 0;
 
 seatsSchemeSvg.addEventListener('click', (event) => {
   if (!event.target.classList.contains('booked')) {
+    console.log(event.target);
     event.target.classList.toggle('active');
     let totalSeats = seatsSchemeSvg.querySelectorAll('.active').length;
     totalCost = cost * totalSeats;
