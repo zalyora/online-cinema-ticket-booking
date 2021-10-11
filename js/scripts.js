1f28494856d3086f903cdcca8299e1824c51079f
zalyora <48105531+zalyora@users.noreply.github.com>
2021-10-11 18:39:47 +0300

    necessary images added, youtube trailer opening lib added

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,22 @@
 'use strict';
 const seatsSchemeSvg = document.querySelector('.seats__scheme__svg');
 const totalPrice = document.querySelector('.total__price');
+const menuButton = document.querySelector('.m__menu');
+const menu = document.querySelector('.nav__menu');
+
 let cost = 100;
 let totalCost = 0;
 
 seatsSchemeSvg.addEventListener('click', (event) => {
   if (!event.target.classList.contains('booked')) {
-    console.log(event.target);
     event.target.classList.toggle('active');
     let totalSeats = seatsSchemeSvg.querySelectorAll('.active').length;
     totalCost = cost * totalSeats;
     totalPrice.innerHTML = totalCost;
   }
 
+});
+
+menuButton.addEventListener('click', () => {
+  menu.classList.toggle('shown');
 });
